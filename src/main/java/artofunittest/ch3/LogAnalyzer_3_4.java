531630a94fb9bc70bfc2b93a7382b29022f29b85
yth00393 <yohan_hsieh@tzuchi.org.tw>
2022-08-03 17:34:50 +0800

    完成 3.4.3 節

---
+++ src/main/java/artofunittest/ch3/LogAnalyzer_3_4.java
@@ -0,0 +1,14 @@
+package artofunittest.ch3;
+
+public class LogAnalyzer_3_4 {
+
+    private IExtensionManager manager;
+
+    public LogAnalyzer_3_4(IExtensionManager manager) {
+        this.manager = manager;
+    }
+
+    public boolean isValidLogFileName(String fileName) {
+        return manager.isValid(fileName);
+    }
+}
