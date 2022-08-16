51667f0bb1b0cad401af155c209af3a2d6a1e608
yth00393 <yohan_hsieh@tzuchi.org.tw>
2022-08-16 15:48:46 +0800

    完成 3.4.6節，工廠方法注入 stub

---
+++ src/main/java/artofunittest/ch3/FileExtensionManager_3_6.java
@@ -0,0 +1,16 @@
+package artofunittest.ch3;
+
+public class FileExtensionManager_3_6 implements IExtensionManager {
+
+    @Override
+    public boolean isValid(String fileName) {
+        if (fileName == null || fileName.isEmpty()) {
+            throw new IllegalArgumentException("filename has to be provided");
+        }
+
+        if (!(fileName.endsWith(".SLF") || fileName.endsWith(".slf"))) {
+            return false;
+        }
+        return true;
+    }
+}
