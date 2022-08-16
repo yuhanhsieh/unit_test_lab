a8b7b6c3a11cd0135aeb86849495758ed60fce3e
yth00393 <yohan_hsieh@tzuchi.org.tw>
2022-08-16 15:50:45 +0800

    加一列

--- src/test/java/artofunittest/ch3/LogAnalyzer_3_5Test.java
+++ src/test/java/artofunittest/ch3/LogAnalyzer_3_5Test.java
@@ -9,6 +9,7 @@ public class LogAnalyzer_3_5Test {
     public void isValidFileName_supportedExtension_returnsTrue() {
         // setup the stub to use, make sure it returns true
         IExtensionManager fakeManager = new FileExtensionManager_3_5();
+
         // create analyzer and inject stub
         LogAnalyzer_3_5 log = new LogAnalyzer_3_5();
         log.setManager(fakeManager);
